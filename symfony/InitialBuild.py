190179b39c1db48e8357c3ca56bdb54a2881000a
Greg Harvey <greg.harvey@gmail.com>
2017-11-14 19:15:01 +0100

    Actually, we can do this with the '-p' flag on mkdir.

--- symfony/InitialBuild.py
+++ symfony/InitialBuild.py
@@ -13,17 +13,12 @@ from Symfony import *
 @roles('app_primary')
 def initial_config(repo, buildtype, build):
   print "===> Looks like a first build, preparing common files and directories..."
-  if not exists("/var/www/shared"):
-    if sudo("mkdir /var/www/shared").failed:
-      raise SystemExit("There was no 'shared' directory on the server and we could not create it either")
-    else:
-      print "==> Created a new /var/www/shared directory for shared assets on this server"
-  if sudo("mkdir /var/www/shared/%s_%s_logs" % (repo, buildtype)).failed:
+  if sudo("mkdir -p /var/www/shared/%s_%s_logs" % (repo, buildtype)).failed:
     raise SystemExit("Could not create logs directory")
-  if sudo("mkdir /var/www/shared/%s_%s_sessions" % (repo, buildtype)).failed:
+  if sudo("mkdir -p /var/www/shared/%s_%s_sessions" % (repo, buildtype)).failed:
     raise SystemExit("Could not create sessions directory")
-  if sudo("mkdir /var/www/shared/%s_%s_data" % (repo, buildtype)).failed:
+  if sudo("mkdir -p /var/www/shared/%s_%s_data" % (repo, buildtype)).failed:
     raise SystemExit("Could not create data directory")
-  if sudo("mkdir /var/www/shared/%s_%s_uploads" % (repo, buildtype)).failed:
+  if sudo("mkdir -p /var/www/shared/%s_%s_uploads" % (repo, buildtype)).failed:
     raise SystemExit("Could not create uploads directory")
   print "===> Directories all set"
