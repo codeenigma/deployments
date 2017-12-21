6b2222e80fa062bcd1f247023ac896d411b3f546
Greg Harvey <greg.harvey@gmail.com>
2017-12-21 11:46:34 +0100

    Adding 'download database' db grabber to DrupalUtils.

--- drupal/DrupalUtils.py
+++ drupal/DrupalUtils.py
@@ -29,3 +29,42 @@ def determine_drupal_version(drupal_version, repo, branch, build, config, method
 
   print "===> Drupal version is D%s." % drupal_version
   return drupal_version
+
+
+# Fetch a fresh database dump from target server
+@task
+def get_database(shortname, branch, santise):
+  # First, check the site exists on target server server
+  if run('drush sa | grep ^@%s_%s$ > /dev/null' % (shortname, branch)).failed:
+    print "ERROR: Could not find a site with the Drush alias %s_%s in order to grab a database dump. Aborting." % (shortname, branch)
+    raise SystemError("Could not find a site with the Drush alias %s_%s in order to grab a database dump. Aborting." % (shortname, branch))
+
+  print "===> Found a site with Drush alias %s_%s. Let's grab a database and copy it down." % (shortname, branch)
+
+  # Make sure a backup directory exists
+  print "===> Make sure a backup directory exists on target server..."
+  run("mkdir -p ~jenkins/client-db-dumps")
+
+  # Let's dump the database into a bzip2 file
+  print "===> Dumping database into bzip2 file..."
+  if santise == "yes":
+    # We need to run a special mysqldump command to obfustcate the database
+    with settings(hide('running', 'stdout', 'stderr')):
+      dbname = run("drush @%s_%s status  Database\ name | awk {'print $4'} | head -1" % (shortname, branch))
+      dbuser = run("drush @%s_%s status  Database\ user | awk {'print $4'} | head -1" % (shortname, branch))
+      dbpass = run("drush @%s_%s --show-passwords status  Database\ pass | awk {'print $4'} | head -1" % (shortname, branch))
+      dbhost = run("drush @%s_%s status  Database\ host | awk {'print $4'} | head -1" % (shortname, branch))
+      run('mysqldump --single-transaction -c --opt -Q --hex-blob -u%s -p%s -h%s %s | /usr/local/bin/drupal-obfuscate.rb | bzip2 -f > ~jenkins/client-db-dumps/%s-%s_database_dump.sql.bz2' % (dbuser, dbpass, dbhost, dbname, shortname, branch))
+  else:
+    run('drush @%s_%s sql-dump | bzip2 -f > ~jenkins/client-db-dumps/%s-%s_database_dump.sql.bz2' % (shortname, branch, shortname, branch))
+
+  # Make sure a directory exists for database dumps to be downloaded to
+  local('mkdir -p /tmp/client-db-dumps')
+
+  # Now fetch it from target server
+  print "===> Fetching database from target server..."
+  get('~/client-db-dumps/%s-%s_database_dump.sql.bz2' % (shortname, branch), '/tmp/client-db-dumps/%s-%s_database_dump.sql.bz2' % (shortname, branch))
+
+  # Remove database dump from target server
+  print "===> Removing database dump from target server..."
+  run('rm ~/client-db-dumps/%s-%s_database_dump.sql.bz2' % (shortname, branch))
