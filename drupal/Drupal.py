468ff30deefca5e085c8f6763c4294b08717cd2a
Emlyn Kinzett <emlyn.kinzett@codeenigma.com>
2018-03-02 16:25:49 +0000

    Reworking the Drupal.drush_fra_branches() function.

--- drupal/Drupal.py
+++ drupal/Drupal.py
@@ -44,62 +44,55 @@ def configure_site_mapping(repo, mapping, config):
 
 @task
 def drush_fra_branches(config, branch):
-  # Default to master and stage to prevent any current jobs using
-  # the previous version of this function from breaking.
-  feature_branches = ['master', 'stage']
-
   # @TODO temporary, this can go once nobody uses [Features] in config.ini any more
   # If a 'branches' option exists in the [Features] section in config.ini, proceed
-  if config.has_option("Features", "branches"):
-    print "############### ===> Fetching feature branches from 'branches' under [Features] in config.ini - DEPRECATED! Please use 'feature_branches' under [Build] instead"
-    feature_branches = []
-    # Get the 'branches' option from under the [Features] section
-    revert_features = config.get("Features", "branches")
-    if revert_features == "*":
-      #just append the current branch
-      feature_branches.append(branch)
+  # THIS IS DEPRECATED
+  feature_branches = common.ConfigFile.return_config_item(config, "Features", "branches", "string", None, True, True, "Drupal")
+
+  if feature_branches is not None:
+    revert_branches = []
+    if feature_branches == "*":
+      revert_branches.append(branch)
     else:
-      # Split the 'branches' option using a comma as a delimiter
-      revert_features = revert_features.split(',')
-      # For each value, strip it and add it to the branches list, which will be searched later
-      for each_branch in revert_features:
+      feature_branches = feature_branches.split(',')
+      for each_branch in feature_branches:
         each_branch = each_branch.strip()
-        feature_branches.append(each_branch)
+        revert_branches.append(each_branch)
+  else:
+    revert_branches = ['master', 'stage']
 
   # If a 'feature_branches' option exists in the [Build] section in config.ini, proceed
-  elif config.has_option("Build", "feature_branches"):
-    print "############### ===> Fetching feature branches from 'feature_branches' under [Build] in config.ini - DEPRECATED! Please use 'feature_branches' under [Drupal] instead"
-    feature_branches = []
-    # Get the 'feature_branches' option from under the [Features] section
-    revert_features = config.get("Build", "feature_branches")
-    if revert_features == "*":
-      #just append the current branch
-      feature_branches.append(branch)
+  # THIS IS DEPRECATED
+  feature_branches = common.ConfigFile.return_config_item(config, "Build", "feature_branches", "string", None, True, True, "Drupal")
+
+  if feature_branches is not None:
+    revert_branches = []
+    if feature_branches == "*":
+      revert_branches.append(branch)
     else:
-      # Split the 'feature_branches' option using a comma as a delimiter
-      revert_features = revert_features.split(',')
-      # For each value, strip it and add it to the branches list, which will be searched later
-      for each_branch in revert_features:
+      feature_branches = feature_branches.split(',')
+      for each_branch in feature_branches:
         each_branch = each_branch.strip()
-        feature_branches.append(each_branch)
+        revert_branches.append(each_branch)
+  else:
+    revert_branches = ['master', 'stage']
 
   # If a 'feature_branches' option exists in the [Drupal] section in config.ini, proceed
-  elif config.has_option("Drupal", "feature_branches"):
-    feature_branches = []
-    # Get the 'feature_branches' option from under the [Features] section
-    revert_features = config.get("Drupal", "feature_branches")
-    if revert_features == "*":
-      #just append the current branch
-      feature_branches.append(branch)
-    else:
-      # Split the 'feature_branches' option using a comma as a delimiter
-      revert_features = revert_features.split(',')
-      # For each value, strip it and add it to the branches list, which will be searched later
-      for each_branch in revert_features:
-        each_branch = each_branch.strip()
-        feature_branches.append(each_branch)
+  feature_branches = common.ConfigFile.return_config_item(config, "Drupal", "feature_branches", "string", None, True, False)
 
-  return feature_branches
+  if feature_branches is not None:
+    revert_branches = []
+    if feature_branches == "*":
+      revert_branches.append(branch)
+    else:
+      feature_branches = feature_branches.split(',')
+      for each_branch in feature_branches:
+        each_branch = each_branch.strip()
+        revert_branches.append(each_branch)
+  else:
+    revert_branches = ['master', 'stage']
+
+  return revert_branches
 
 
 # Take a database backup
