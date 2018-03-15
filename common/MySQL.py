742055da9f10702ab21daedef4bad8ff1a09f36f
Greg Harvey <greg.harvey@gmail.com>
2018-03-15 18:59:51 +0100

    #86 - adding generic MySQL db backup function to common/MySQL.py.

--- common/MySQL.py
+++ common/MySQL.py
@@ -118,3 +118,24 @@ def mysql_import_dump(site_root, db_name, dump_file, db_host=None, rds=False, my
   # Put the correct host back for Fabric to continue
   if not db_host == "localhost" or rds:
     env.host = original_host
+
+
+# Take a database backup
+@task
+@roles('app_primary')
+def mysql_backup_db(db_name, build, fail_build=False, mysql_config='/etc/mysql/debian.cnf'):
+  print "===> Ensuring backup directory exists"
+  with settings(warn_only=True):
+    if run("mkdir -p ~jenkins/dbbackups").failed:
+      raise SystemExit("######### Could not create directory ~jenkins/dbbackups! Aborting early")
+  print "===> Taking a database backup..."
+  with settings(warn_only=True):
+    if sudo("mysql-dump --defaults-file=%s %s | gzip > ~jenkins/dbbackups/%s_prior_to_%s.sql.gz; if [ ${PIPESTATUS[0]} -ne 0 ]; then exit 1; else exit 0; fi" % (mysql_config, db_name, db_name, build)).failed:
+      failed_backup = True
+    else:
+      failed_backup = False
+
+  if failed_backup and fail_build:
+    raise SystemExit("######### Could not take database backup prior to launching new build! Aborting early")
+  if failed_backup:
+    print "######### Backup failed, but build set to continue regardless"
