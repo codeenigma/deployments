0c485bb80e097305c6eaaef4abaf34885411f74e
gregharvey <greg.harvey@gmail.com>
2018-06-01 15:18:49 +0200

    Wrong variable name in string checker.

--- drupal/fabfile-drushcommand.py
+++ drupal/fabfile-drushcommand.py
@@ -42,7 +42,7 @@ def main(shortname, branch, command, backup=True):
 
   BLACKLISTED_CMDS = ['sql-drop', 'site-install', 'si', 'sudo', 'rm', 'shutdown', 'reboot', 'halt', 'chown', 'chmod', 'cp', 'mv', 'nohup', 'echo', 'cat', 'tee', 'php-eval', 'variable-set', 'vset']
   blacklisted = False
-  blacklisted = common.Utils.detect_malicious_strings(BLACKLISTED_CMDS, cmd)
+  blacklisted = common.Utils.detect_malicious_strings(BLACKLISTED_CMDS, command)
   if blacklisted:
     raise SystemError("Surely you jest... I won't run drush @%s_%s %s. Ask a sysadmin instead." % (shortname, branch, command))
 
