e67c75115f87a78e8a929fc84f8475f1059aeb99
Emlyn Kinzett <emlyn.kinzett@codeenigma.com>
2019-09-11 12:55:49 +0100

    Add the ability to mark a build unstable if composer.lock is outdated.

--- common/PHP.py
+++ common/PHP.py
@@ -10,7 +10,7 @@ import common.Utils
 # Run a composer command
 @task
 @roles('app_all')
-def composer_command(site_root, composer_command="install", package_to_install=None, install_no_dev=True, composer_lock=True, composer_global=False, composer_sudo=False, symfony_environment=None, through_ssh=False):
+def composer_command(site_root, composer_command="install", package_to_install=None, install_no_dev=True, composer_lock=True, composer_global=False, composer_sudo=False, symfony_environment=None, through_ssh=False, mark_unstable=False):
   # Make sure no one passed anything nasty from a build hook
   malicious_code = False
   malicious_code = common.Utils.detect_malicious_strings([';', '&&'], composer_command)
@@ -61,3 +61,22 @@ def composer_command(site_root, composer_command="install", package_to_install=N
   else:
     # If `through_ssh` is not set, run the Composer command as normal on the destination server.
     run(this_command)
+
+@task
+@roles('app_all')
+def composer_validate(site_root, through_ssh):
+  validate_command = "cd %s; composer validate --no-check-all --no-check-publish"
+  composer_lock_outdated = False
+  with settings(warn_only=True):
+    if through_ssh:
+      composer_validate_output = common.Utils._sshagent_run(validate_command)
+    else:
+      composer_validate_output = run(validate_command)
+
+    if run("echo \"%s\" | grep 'The lock file is not up to date with the latest changes'" % composer_validate_output).failed:
+      print "composer.lock is up to date with composer.json"
+    else:
+      print "composer.lock is not up to date with the latest changes in composer.json. Mark build as unstable."
+      composer_lock_outdated = True
+
+  return composer_lock_outdated
