23b1d2386e844c25a8d5d573de3dd4c892aff486
Emlyn Kinzett <emlyn.kinzett@codeenigma.com>
2019-11-25 17:38:14 +0000

    Missed an 's' in roles.

--- drupal/Drupal.py
+++ drupal/Drupal.py
@@ -119,7 +119,7 @@ def get_db_name(repo, branch, build, buildtype, site, drush_output):
 
 # Get the database username of an existing Drupal website
 @task
-@role('app_primary')
+@roles('app_primary')
 def get_db_user(repo, branch, site, drush_output):
   db_user = None
   db_user = run("echo \"%s\" | grep \"db-username:\" | cut -d \":\" -f 2" % drush_output)
