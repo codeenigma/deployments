5a8f478b57d9200aeb43b318527cc53245cfa533
Emlyn Kinzett <emlyn.kinzett@codeenigma.com>
2020-02-17 13:16:04 +0000

    Create an option that can be used to disable the installation of phpunit.

--- common/Tests.py
+++ common/Tests.py
@@ -11,7 +11,7 @@ import common.PHP
 # Run phpunit tests
 @task
 @roles('app_primary')
-def run_phpunit_tests(path_to_app, group='unit', path='www', phpunit_path='vendor/phpunit/phpunit/phpunit'):
+def run_phpunit_tests(path_to_app, group='unit', path='www', phpunit_path='vendor/phpunit/phpunit/phpunit', phpunit_install=True):
   phpunit_tests_failed=True
   # We cannot make phpunit work with PHP 5.x, too many problems
   # Detect PHP version
@@ -23,8 +23,12 @@ def run_phpunit_tests(path_to_app, group='unit', path='www', phpunit_path='vendo
   with cd("%s" % path_to_app):
     # Make sure phpunit is available to use
     # We don't want to fail if it's already there
-    with settings(warn_only=True):
-      common.PHP.composer_command(path_to_app, "require", "phpunit/phpunit")
+    if phpunit_install:
+      with settings(warn_only=True):
+        print "===> Installing phpunit..."
+        common.PHP.composer_command(path_to_app, "require", "phpunit/phpunit")
+    else:
+      print "##### phpunit already installed."
 
     # Now let's look for a phpunit.xml file to use
     phpunit_xml = False
