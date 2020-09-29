5d681709bd56a44051330a2764a511eff7469f49
Matthieu Decorniquet <matthieu.decorniquet@codeenigma.com>
2020-09-29 18:29:31 +0200

    make it so wp initial build take the RDS host into account

--- util/mysqlpreparenoimport_wp.sh
+++ util/mysqlpreparenoimport_wp.sh
@@ -9,6 +9,7 @@
 myFile='/etc/mysql/debian.cnf'
 myUser=`grep user $myFile | awk '{print $3}' | uniq`
 myPass=`grep password $myFile | awk '{print $3}' | uniq`
+myHost=`grep host $myFile | awk '{print $3}' | uniq`
 
 
 # Args that must be passed
@@ -47,7 +48,7 @@ fi
 ORIGDB=$NEWDB
 
 while [  $COUNTER -lt 99 ]; do
-  DB=`mysql -u$myUser -p$myPass -Bse 'show databases'| egrep "^${NEWDB}$"`
+  DB=`mysql -u$myUser -p$myPass -h$myHost -Bse 'show databases'| egrep "^${NEWDB}$"`
   if [ "$DB" = "$NEWDB" ]; then
     echo "The database $NEWDB already exists"
     let COUNTER=COUNTER+1
@@ -55,14 +56,14 @@ while [  $COUNTER -lt 99 ]; do
     continue
   else
     echo "Creating a database for $NEWDB"
-    mysqladmin -u$myUser -p$myPass create $NEWDB || exit 1
+    mysqladmin -u$myUser -p$myPass -h$myHost create $NEWDB || exit 1
     echo "Generating a user/pass for database $NEWDB"
-    mysql -u$myUser -p$myPass -e "GRANT ALL ON \`$NEWDB\`.* to '$NEWDB'@'localhost' IDENTIFIED BY '$PASS'" || exit 1
+    mysql -u$myUser -p$myPass -h$myHost -e "GRANT ALL ON \`$NEWDB\`.* to '$NEWDB'@'localhost' IDENTIFIED BY '$PASS'" || exit 1
     break
   fi
 done
 
-DB=`mysql -u$myUser -p$myPass -Bse 'show databases'| egrep "^${NEWDB}$"`
+DB=`mysql -u$myUser -p$myPass -h$myHost -Bse 'show databases'| egrep "^${NEWDB}$"`
 # Only do any importing if the database exists ( we should have failed above
 if [ $? -eq 0 ]; then
   #########################################################################
@@ -83,7 +84,7 @@ if [ $? -eq 0 ]; then
   #
   #########################################################################
 
-  wp --path=$SITE_ROOT --allow-root core config --dbname=$NEWDB --dbuser=$NEWDB --dbpass=$PASS
+  wp --path=$SITE_ROOT --allow-root core config --dbname=$NEWDB --dbuser=$NEWDB --dbpass=$PASS --dbhost=$myHost
   wp --path=$SITE_ROOT --allow-root core install --url=$URL --title=$NEWDB --admin_user=codeenigma --admin_email=sysadm@codeenigma.com --admin_password=$PASS
 else
   echo "Something went wrong and we couldn't import a database or populate wp-config.php. Maybe we couldn't create the database?"
