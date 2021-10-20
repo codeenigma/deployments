530b37a78c79457ae322009ad8b437325d740cd9
matason <chrispmaiden@gmail.com>
2021-10-20 16:14:00 +0100

    Enable through_ssh option for Symfony builds

--- symfony/fabfile.py
+++ symfony/fabfile.py
@@ -66,6 +66,7 @@ def main(repo, repourl, branch, build, buildtype, siteroot, keepbuilds=10, url=N
   composer = common.ConfigFile.return_config_item(config, "Composer", "composer", "boolean", True)
   composer_lock = common.ConfigFile.return_config_item(config, "Composer", "composer_lock", "boolean", True)
   no_dev = common.ConfigFile.return_config_item(config, "Composer", "no_dev", "boolean", with_no_dev)
+  through_ssh = common.ConfigFile.return_config_item(config, "Composer", "through_ssh", "boolean", False)
   optimize_autoloader = common.ConfigFile.return_config_item(config, "Composer", "optimize_autoloader", "boolean", no_dev)
 
   # Can be set in the config.ini [AWS] section
@@ -150,7 +151,7 @@ def main(repo, repourl, branch, build, buildtype, siteroot, keepbuilds=10, url=N
     if composer:
       # Generally we want to run with SYMFONY_ENV=prod because dev just enables developer tools
       # If someone wants to override this, we can pass "dev" as buildtype_override above
-      execute(common.PHP.composer_command, site_root, "install", None, no_dev, composer_lock, False, False, console_buildtype, optimize_autoloader)
+      execute(common.PHP.composer_command, site_root, "install", None, no_dev, composer_lock, False, False, console_buildtype, optimize_autoloader, through_ssh=through_ssh)
 
   if migrations:
     execute(Symfony.run_migrations, repo, buildtype, build, console_buildtype)
